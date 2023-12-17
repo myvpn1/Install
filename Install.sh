2d01f2c13a65b934ca9c10596991fb7e1c2393fc
myvpn1 <137178860+myvpn1@users.noreply.github.com>
2023-12-18 02:41:18 +0700

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,27 +1,60 @@
 #!/bin/bash
 clear
+# COLOR VALIDATION
+clear
+y='\033[1;33m' #yellow
+l='\033[0;37m'
+BGX="\033[42m"
+CYAN="\033[96m"
+z="\033[96m"
+RED='\033[0;31m'
+NC='\033[0m'
+gray="\e[1;30m"
+Blue="\033[0;34m"
+green='\033[0;32m'
+grenbo="\e[92;1m"
+purple="\033[1;95m"
+YELL='\033[0;33m'
 ###########
 LURUS="\033[1;32m=\033[1;33m=\033[1;31m=\033[1;31m$NC"
 LURUS2="\033[1;32m=\033[1;33m=\033[1;31m=\033[1;31m$NC"
 KANAN="\033[1;32m<\033[1;33m<\033[1;31m<\033[1;31m$NC"
 KIRI="\033[1;32m>\033[1;33m>\033[1;31m>\033[1;31m$NC"
 ########
-echo -e " ${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}"
-echo -e "         • MENU INSTALLER SCRIPTS AUTO INSTALL •         "
-echo -e " ${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}"
-echo -e "" 
-echo -e " ${KIRI} Status Bot ( ${status_bot} )"
+echo -e " ${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LUR>
+echo -e "          SCRIPT AUTO INSTALL 2 VERSI                 " | lolcat
+echo -e " ${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LUR>
 echo -e ""
-echo -e " [\e[36m•1\e[0m] Install Script Stunell"
-echo -e " [\e[36m•2\e[0m] Install Scripts Happyproxy"
-echo -e "" 
-echo -e "" 
-echo -e " ${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}"
 echo -e ""
-read -p " Select menu :  "  opt
+echo -e " ${RED}╭━━━┳━━━┳━━━┳━━┳━━━┳━━━━┳━━━╮╭━━━┳╮╱╭┳━━━━┳━━━╮${NC}"
+echo -e " ${RED}┃╭━╮┃╭━╮┃╭━╮┣┫┣┫╭━╮┃╭╮╭╮┃╭━╮┃┃╭━╮┃┃╱┃┃╭╮╭╮┃╭━╮┃${NC}"                                                                                   echo -e " ${RED}┃╰━━┫┃╱╰┫╰━╯┃┃┃┃╰━╯┣╯┃┃╰┫╰━━╮┃┃╱┃┃┃╱┃┣╯┃┃╰┫┃╱┃┃${NC}"
+echo -e " ${RED}╰━━╮┃┃╱╭┫╭╮╭╯┃┃┃╭━━╯╱┃┃╱╰━━╮┃┃╰━╯┃┃╱┃┃╱┃┃╱┃┃╱┃┃${NC}"
+echo -e " ${RED}┃╰━╯┃╰━╯┃┃┃╰┳┫┣┫┃╱╱╱╱┃┃╱┃╰━╯┃┃╭━╮┃╰━╯┃╱┃┃╱┃╰━╯┃${NC}"
+echo -e " ${RED}╰━━━┻━━━┻╯╰━┻━━┻╯╱╱╱╱╰╯╱╰━━━╯╰╯╱╰┻━━━╯╱╰╯╱╰━━━╯${NC}"
+echo -e " ${RED}╭━━┳━╮╱╭┳━━━┳━━━━┳━━━┳╮╱╱╭╮${NC}"
+echo -e " ${RED}╰┫┣┫┃╰╮┃┃╭━╮┃╭╮╭╮┃╭━╮┃┃╱╱┃┃${NC}"
+echo -e " ${RED}╱┃┃┃╭╮╰╯┃╰━━╋╯┃┃╰┫┃╱┃┃┃╱╱┃┃${NC}"
+echo -e " ${RED}╱┃┃┃┃╰╮┃┣━━╮┃╱┃┃╱┃╰━╯┃┃╱╭┫┃╱╭╮${NC}"
+echo -e " ${RED}╭┫┣┫┃╱┃┃┃╰━╯┃╱┃┃╱┃╭━╮┃╰━╯┃╰━╯┃${NC}"
+echo -e " ${RED}╰━━┻╯╱╰━┻━━━╯╱╰╯╱╰╯╱╰┻━━━┻━━━╯${NC}"
+echo -e ""
+echo -e ""
+echo -e " [\e[36m•1\e[0m] 𝙄𝙉𝙎𝙏𝘼𝙇𝙇 𝙎𝘾𝙍𝙄𝙋𝙏𝙎 𝙎𝙏𝙐𝙉𝙀𝙇𝙇" | lolcat
+echo -e " [\e[36m•2\e[0m] 𝗜𝗡𝗦𝗧𝗔𝗟𝗟 𝗦𝗖𝗥𝗜𝗣𝗧𝗦 𝗛𝗔𝗣𝗣𝗬𝗣𝗥𝗢𝗫𝗬" | lolcat
+echo -e ""
+echo -e ""
+echo -e " Owner = Andyyuda " | lolcat
+echo -e " Beli Lisensinya kak wa 082131861788" | lolcat
+echo -e " 1BULAN 1IP = 10K" | lolcat
+echo -e " 3BULAN 1IP = 20K" | lolcat
+echo -e " LIMFITIME 1IP = 50" |lolcat
+echo -e " UNLI IP = CHAT SAYA " | lolcat
+echo -e " ${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LURUS}${LURUS2}${LUR>
+echo -e ""
+read -p " Select Script nya Kak :  "  opt
 echo -e   ""
 case $opt in
-01 | 1) clear ; apt update && apt upgrade -y && update-grub && sleep 2 && sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt upgrade && apt install -y bzip2 gzip coreutils screen curl unzip && wget -q https://raw.githubusercontent.com/myvpn1/v5/main/setup.sh && chmod +x setup.sh && ./setup.sh ;;
-02 | 2) clear ; hapus-bot ;;
+01 | 1) clear ; apt update && apt upgrade -y && update-grub && sleep 2 && sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default>
+02 | 2) clear ; apt install -y && apt update -y && apt upgrade -y && apt install lolcat -y && gem install lolcat && wget -q https://raw.githubuserconte>
 *) clear ; exit ;;
 esac
